bc27866ba733f646ad16cfa832b8f27811b95015
Jin <stormjin04@gmail.com>
2023-11-14 18:14:25 -0800

    feat: integrated config

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Carousel images={images} />
+        <Carousel images={images} config={{}} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/common/carousel/Carousel.tsx
+++ src/components/common/carousel/Carousel.tsx
@@ -3,29 +3,23 @@ import "./Carousel.scss";
 import { useEffect } from "react";
 import { gsap } from "gsap";
 
+interface ConfigType {}
+
 interface CarouselType {
   images: string[];
+  config?: ConfigType;
 }
 
-export const Carousel: React.FC<CarouselType> = ({ images }) => {
+export const Carousel: React.FC<CarouselType> = ({ images, config }) => {
   useEffect(() => {
     const boxes: HTMLDivElement[] = gsap.utils.toArray(".carousel-item");
-    // const colors = ["#f38630", "#6fb936", "#ccc", "#6fb936"];
 
     if (boxes.length > 0) {
-      // gsap.set(boxes, {
-      //   backgroundColor: gsap.utils.wrap(colors),
-      // });
       const loop = horizontalLoop(boxes, { paused: true, draggable: true });
-      // boxes.forEach((box, i) =>
-      //   box.addEventListener("click", () =>
-      //     loop.toIndex(i, { duration: 0.8, ease: "power1.inOut" })
-      //   )
-      // );
       console.log("loop: ", loop);
-      
     }
   }, [images]);
+
   return (
     <div className="carousel">
       <div className="carousel-inner">
